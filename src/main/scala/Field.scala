f90ec78915250cf6e60dde28d5cc8f6b6c654f9e
Simon Winter <simonwinter.1@gmx.de>
2021-04-09 10:38:36 +0200

    Creating playfield and adding first row

--- src/main/scala/Field.scala
+++ src/main/scala/Field.scala
@@ -1,2 +1,13 @@
 case class Field()
 
+val playfield = Array.ofDim[String](8,8)
+
+playfield(0)(0) = "A8"
+playfield(1)(0) = "B8"
+playfield(2)(0) = "C8"
+playfield(3)(0) = "D8"
+
+playfield(4)(0) = "E8"
+playfield(5)(0) = "F8"
+playfield(6)(0) = "G8"
+playfield(7)(0) = "H8"
