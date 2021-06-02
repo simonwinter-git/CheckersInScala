c5de55f8be7dbed6e80ffc2109374632106a85f5
Yannick <yannickschall@gmx.de>
2021-06-02 23:27:13 +0200

    Fixed most things

--- src/test/scala/aview/TuiSpec.scala
+++ src/test/scala/aview/TuiSpec.scala
@@ -7,12 +7,12 @@ class TuiSpec extends AnyWordSpec {
   "A tui" should {
     val controller = new Controller(new GameBoard(8))
     val tui = new Tui(controller)
-    "create us a 8x8 gameboard when typing 'new8' " in {
-      tui.tuiEntry("new8")
+    "create us a 8x8 gameboard when typing 'new 8' " in {
+      tui.tuiEntry("new 8")
       controller.gameBoard should be (new GameBoard(8))
     }
-    "create us a 10x10 gameboard when typing 'new10' " in {
-      tui.tuiEntry("new10")
+    "create us a 10x10 gameboard when typing 'new 10' " in {
+      tui.tuiEntry("new 10")
       controller.gameBoard should be(new GameBoard(10))
     }
   }
